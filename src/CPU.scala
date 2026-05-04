c40c580799c7cc0cf649c0618666a88cd0e5a7c6
Yu Jin <jinyu929@qq.com>
2026-05-04 20:57:30 +0800

    feat: add plic & jtag

--- src/CPU.scala
+++ src/CPU.scala
@@ -20,6 +20,15 @@ class rapt extends BlackBox {
     val io_interrupt = Input(Bool())
     val io_master = AXI4Bundle(CPUAXI4BundleParameters())
     val io_slave = Flipped(AXI4Bundle(CPUAXI4BundleParameters()))
+    // External IRQ vector into the cluster PLIC (sources 1..31). Tied off in
+    // Impl below; ysyxSoC has no PLIC source aggregator.
+    val ext_irq_i = Input(UInt(31.W))
+    // JTAG / RISC-V Debug ports. Always present on rapt; tied to TLR-park
+    // here so the DTM stays parked (no JTAG header on ysyxSoC).
+    val jtag_trst_n = Input(Bool())
+    val jtag_tms   = Input(Bool())
+    val jtag_tdi   = Input(Bool())
+    val jtag_tdo   = Output(Bool())
   })
 }
 
@@ -41,5 +50,11 @@ class CPU(idBits: Int)(implicit p: Parameters) extends LazyModule {
     cpu.io.io_interrupt := interrupt
     cpu.io.io_slave <> slave
     master <> cpu.io.io_master
+    // External IRQ aggregator not present in ysyxSoC; tie off.
+    cpu.io.ext_irq_i := 0.U
+    // JTAG: park DTM in TLR via trst_n=0, tms=1, tdi=0; tdo is unused.
+    cpu.io.jtag_trst_n := false.B
+    cpu.io.jtag_tms    := true.B
+    cpu.io.jtag_tdi    := false.B
   }
 }
